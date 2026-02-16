96dd840dd7cee4c2f54b9d0174a425e952d5f3c6
hema <hema123@gmail.com>
2026-02-16 11:51:36 +0530

    modified by feature1

--- grade.py
+++ grade.py
@@ -9,3 +9,7 @@ elif num>=60:
     print("Grade: D")
 else:
     print("Grade: F")
+    if num<0 or num>100:
+        print("Invalid marks entered. Please enter a number between 0 and 100.")    
+    else:
+        print("Your grade is F. Better luck next time!")
