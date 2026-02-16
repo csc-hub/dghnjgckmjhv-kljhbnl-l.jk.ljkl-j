91aab63817bccd0a2cfd18bd75f1a5240328a4ba
hema <hema123@gmail.com>
2026-02-16 11:48:07 +0530

    grade Program

---
+++ grade.py
@@ -0,0 +1,11 @@
+num=int(input("Enter your marks: "))
+if num>=90:
+    print("Grade: A")
+elif num>=80:
+    print("Grade: B")
+elif num>=70:
+    print("Grade: C")
+elif num>=60:
+    print("Grade: D")
+else:
+    print("Grade: F")
